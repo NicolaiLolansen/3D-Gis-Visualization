a1ef58b82bda2d1a0d093041589b56991d8eba85
Adam Emil Paltorp Schmitt <s134873@student.dtu.dk>
2016-03-10 09:52:14 +0100

    parser prototype

--- 3D-Gis-Visualization/3D-Gis-Visualization/3D/dat-gui_panel.js
+++ 3D-Gis-Visualization/3D-Gis-Visualization/3D/dat-gui_panel.js
@@ -158,19 +158,12 @@ Q3D.gui = {
       var parameters = this.parameters;
       var funcFolder = this.gui.addFolder('Functions');
 
-         /*
-            This is where a search function should be called/added
-            The user input for FOT-parameter can be accessed with: parameters.FOTsearch
-            Right now a simple function has been made to print the FOT
-            Should be a function call with parameters.FOTsearch as argument
-
-            Jeg har ændret strukturen, det andet virkede ikke rigtigt (Har kopieret den fra wireframe mode, og nu virker det)
-          */
 
       funcFolder.add(this.parameters, 'FOTsearch').name('Search for FOT').onFinishChange(function (value) {
           Q3D.application.searchBuilding(value); //Kalder til qgis2threejs.js med værdien fra feltet
       });
       
+      /*
       funcFolder.add(this.parameters, 'Source').name('Select Data Source').onFinishChange(function (value) {
           Papa.parse(value, {
               download: true,
@@ -183,7 +176,12 @@ Q3D.gui = {
               }
           })
       })
-
+      */
+      funcFolder.add(this.parameters, 'Source').name('Select Data Source').onFinishChange(function (value) {
+          addSource(value);
+          startParse();
+          console.log(getAllData());
+      })
   },
 
   addHelpButton: function () {
---
+++ 3D-Gis-Visualization/3D-Gis-Visualization/3D/parser.js
@@ -0,0 +1,61 @@
+﻿parseconfig = {
+        delimiter: "",
+        newline: "",
+        header: true,
+        dynamicTyping: false,
+        preview: 0,
+        encoding: "",
+        worker: false,
+        comments: false,
+        step: undefined,
+        complete: parseComplete,
+        error: undefined,
+        download: true,
+        skipEmptyLines: false,
+        chunk: undefined,
+        fastMode: undefined,
+        beforeFirstChunk: undefined,
+        withCredentials: undefined
+}
+
+data = {
+    sources: [],
+    outputs: []
+}
+
+function addSource(url) {
+    data.sources.push(url);
+}
+
+function removeSource(url) {
+    var index = data.sources.indexOf(url);
+    if (index > -1) {
+        data.sources.splice(index, 1);
+    }
+}
+
+function parseComplete(results) {
+    console.log(results);
+    data.outputs.push(results.data);
+}
+
+function startParse() {
+    data.sources.forEach(function (entry) {
+        singleParse(entry)
+    });
+}
+
+function singleParse(source){
+    Papa.parse(source, parseconfig);
+}
+
+function getAllData() {
+    return data.outputs;
+}
+
+function getData(type) {
+    var index = data.sources.indexOf(type);
+    if (index > -1) {
+        data.sources.splice(index, 1);
+    }
+}
