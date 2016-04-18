5228f779677be2e5f343b7633866072c23942bc5
milleniana <s134873@student.dtu.dk>
2016-04-18 09:38:46 +0200

    color buildings

--- 3D-Gis-Visualization/3D-Gis-Visualization/3D/dat-gui_panel.js
+++ 3D-Gis-Visualization/3D-Gis-Visualization/3D/dat-gui_panel.js
@@ -16,10 +16,11 @@ Q3D.gui = {
     },
     i: Q3D.application.showInfo,
     FOTsearch: '0000000000',
-    Source: 'https://dl.dropboxusercontent.com/s/8qyigf5hvqmty0z/csvtest1.csv?dl=1',
+    Source: 'https://dl.dropboxusercontent.com/s/8qyigf5hvqmty0z/csvtest1.csv',
     getParseResult: getAllData,
     getParseSources: getSources,
-    getbounds: "http://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:storbyhaver&outputFormat=json"
+    getbounds: "http://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:storbyhaver&outputFormat=json",
+    showColor: false
   },
 
   // initialize gui
@@ -91,7 +92,7 @@ Q3D.gui = {
             project.layers[this.object.i].setSideVisibility(value);
           });
           break;
-        }
+        }   
       }
 
       folder.add(parameters.lyr[i], 'o').min(0).max(1).name('Opacity').onChange(opacityChanged);
@@ -168,27 +169,15 @@ Q3D.gui = {
       
       funcFolder.add(this.parameters, 'getbounds').name('Get Bounds!').onFinishChange(function (value) { Q3D.application.getbounds(value) }); //Kalder til qgis2threejs.js med værdien fra feltet
 
-      /*
-      funcFolder.add(this.parameters, 'Source').name('Select Data Source').onFinishChange(function (value) {
-          Papa.parse(value, {
-              download: true,
-              header: true,
-              error: function(error, file){
-                  alert(error);
-              },
-              complete: function(results) {
-                console.log("Parsing complete:", results);
-                  Q3D.application.csvResults = results;
-              }
-          })
-      })
-      */
       funcFolder.add(this.parameters, 'Source').name('Select Data Source').onFinishChange(function (value) {
           addSource(value);
       }),
 
       funcFolder.add(this.parameters, 'getParseResult').name('Retrieve Data');
-      funcFolder.add(this.parameters, 'getParseSources').name('Retrieve Sources').onChange(function () {console.log(getSources())});
+      funcFolder.add(this.parameters, 'getParseSources').name('Retrieve Sources').onChange(function () { console.log(getSources()) });
+      funcFolder.add(this.parameters, 'showColor').name('Visualise Using Color').onChange(function () {
+          test();
+      });
   },
 
   addHelpButton: function () {
