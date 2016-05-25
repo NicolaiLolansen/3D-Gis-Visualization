e88c422350949cb16ff8e53767f14ce7345f4ec3
Emil <s134873@student.dtu.dk>
2016-05-25 17:38:24 +0200

    Dynamic scene loading

--- 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/loginscripts.js
+++ 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/loginscripts.js
@@ -28,3 +28,41 @@ function validate() {
 	});
 	console.log("Validation finished");
 }
+
+function getScenes(){
+    $.getJSON({
+        url: 'http://localhost:8081/savesList/',
+        type: 'GET',
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8",
+        header: { 'Access-Control-Allow-Origin': '*' },
+
+        success: function (scene_list) {
+            console.log(scene_list);
+            var element = document.getElementById("button_panel");
+            for (var scene in scene_list.saves) {
+                var name = scene_list.saves[scene];
+                var button = document.createElement("BUTTON");
+                var text = document.createTextNode(name);
+                button.id = name;
+                button.name = name;
+                button.type = "button";
+                button.onClick = function () {
+                    window.sessionStorage.scene = this.name;
+                    window.location = '../3D/index.html';
+                };
+                button.appendChild(text);
+                element.appendChild(button);
+                console.log(button);
+            }
+        },
+        error: function (err) {
+            console.log("error:")
+            console.log(err);
+        }
+    });
+}
+
+function loadScene(scene_name) {
+    console.log(scene_name);
+}
