35b34e2bc0f0faeb67468091f87cca419a7a1cd2
milleniana <s134873@student.dtu.dk>
2016-05-27 11:32:07 +0200

    Dialogboks kommer nu kun første gang

--- 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/index.js
+++ 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/index.js
@@ -3,7 +3,10 @@ var firstClick = true;
 $('.obs').click(function () {
     if (firstClick) {
         if (confirm("You will only be able to view projects, but not edit them. Continue?")) {
+            firstClick = false;
             $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
-        }
+        } 
+    } else {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
     }
 });
