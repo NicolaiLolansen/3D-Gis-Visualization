98d5a907b5977002438e9be9fae76d034ab1dc1f
Emil <s134873@student.dtu.dk>
2016-06-03 17:10:10 +0200

    added file menufunctions.js for datgui and save/load related functions

--- 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/initscripts.js
+++ 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/initscripts.js
@@ -1,4 +1,4 @@
-﻿initScene = function () {
+﻿var initScene = function () {
     var url = 'https://api-geovizjs.rhcloud.com/loadScene/';
     var _scene = 'default.json'; //Name of default scene
     if (window.sessionStorage.userRole == "builder") {
@@ -24,12 +24,53 @@
         }
     });
 }
-initModal = function () {
-    var street = document.getElementById('select_street');
-    var num = document.getElementById('select_streetnum');
-    var zip = document.getElementById('select_zip');
-    
-    for (var key in app.attributeList) {
-       
+var initModal = function () {
+    // Get the modal
+    var modal = document.getElementById('modal-menu');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function () {
+        startCorrelation('https://dl.dropboxusercontent.com/s/88vgr6io5q63cjg/energimaerke.csv'); //ONLY FOR TESTING
     }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+var startCorrelation = function (source_url) {
+    var url = 'http://localhost:8085/getSourceHeaders?targetURL=' + source_url;
+    console.log("performing get request");
+    $.ajax({
+        url: source_url,
+        type: 'GET',
+        dataType: 'json',
+        success: function (headerList) {
+            console.log(headerList);
+            var modal = document.getElementById('modal-menu');
+            var street = document.getElementById('select_street');
+            var num = document.getElementById('select_streetnum');
+            var zip = document.getElementById('select_zip');
+
+            modal.style.display = "block";
+        },
+        error: function (err) {
+            console.log('ERROR');
+            console.log(err);
+        }
+    });
 }
