57765b0dc0f686588826d5a2ea233d1708cc80b3
milleniana <s134873@student.dtu.dk>
2016-06-04 12:47:31 +0200

    Coffee added. And some address building functionality.

--- 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/initscripts.js
+++ 3D-Gis-Visualization/3D-Gis-Visualization/Website/js/initscripts.js
@@ -25,19 +25,14 @@
     });
 }
 var initModal = function () {
+
+
+
     // Get the modal
     var modal = document.getElementById('modal-menu');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function () {
-        startCorrelation('https://dl.dropboxusercontent.com/s/88vgr6io5q63cjg/energimaerke.csv'); //ONLY FOR TESTING
-    }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
@@ -46,8 +41,38 @@ var initModal = function () {
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+
+    }
+
+    //--------------------------------------- TESTING FUNCTIONS ------------------------------------
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function () {
+        startCorrelation('https://dl.dropboxusercontent.com/s/88vgr6io5q63cjg/energimaerke.csv'); //ONLY FOR TESTING
+    }
+    //----------------------------------------------------------------------------------------------
+    
+    var buildbtn = document.getElementById('build');
+    buildbtn.onclick = function () {
+        var street = document.getElementById('select_street');
+        var num = document.getElementById('select_streetnum');
+        var zip = document.getElementById('select_zip');
+
+        var street_sel = street.options[street.selectedIndex].value;
+        var num_sel = num.options[num.selectedIndex].value;
+        var zip_sel = zip.options[zip.selectedIndex].value; 
+
+        if (street_sel == "default" || num_sel == "default" || zip_sel == "default") {
+            alert("Argument missing");
+        } else {
+            var block = {
+                street: street_sel,  //document.getElementById('select_street').value,
+                num: num_sel,        //document.getElementById('select_streetnum').value,
+                zip: zip_sel         //document.getElementById('select_zip').value
+            }
+            console.log(block);
         }
     }
 }
@@ -56,16 +81,21 @@ var startCorrelation = function (source_url) {
     var url = 'http://localhost:8085/getSourceHeaders?targetURL=' + source_url;
     console.log("performing get request");
     $.ajax({
-        url: source_url,
+        url: 'http://localhost:8085/getSourceHeaders/?targetURL=https://dl.dropboxusercontent.com/s/88vgr6io5q63cjg/energimaerke.csv',
         type: 'GET',
         dataType: 'json',
-        success: function (headerList) {
-            console.log(headerList);
+        success: function (json) {
+            console.log("SUCCESS");
             var modal = document.getElementById('modal-menu');
             var street = document.getElementById('select_street');
             var num = document.getElementById('select_streetnum');
             var zip = document.getElementById('select_zip');
 
+            for (var key in json) {
+                street.options.add(new Option(json[key], json[key]));
+                num.options.add(new Option(json[key], json[key]));
+                zip.options.add(new Option(json[key], json[key]));
+            }
             modal.style.display = "block";
         },
         error: function (err) {
@@ -74,3 +104,18 @@ var startCorrelation = function (source_url) {
         }
     });
 }
+
+var startBuild = function () {
+    console.log("bygger")
+    /*
+    var x = document.getElementById("select_street").selectedIndex;
+    var y = document.getElementById("select_streetnum").selectedIndex;
+    var z = document.getElementById("select_zip").selectedIndex;
+    
+    var block = {
+        street: $('select[name=select_street]').val(),  //document.getElementById('select_street').value,
+        num: $('select[name=select_streetnum]').val(),  //document.getElementById('select_streetnum').value,
+        zip: $('select[name=select_zip]').val()         //document.getElementById('select_zip').value
+    }*/
+}
+
